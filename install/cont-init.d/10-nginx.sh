08f653b7c82c566650ba030113b8910e7ee17e70
Dave Conroy <dave@tiredofit.ca>
2017-08-22 19:47:56 -0700

    Based on tiredofit/nginx-php-fpm-7.1-2.5

--- install/cont-init.d/10-nginx.sh
+++ install/cont-init.d/10-nginx.sh
@@ -4,6 +4,6 @@
   UPLOAD_MAX_SIZE=${UPLOAD_MAX_SIZE:="2G"}
   PHP_TIMEOUT=${PHP_TIMEOUT:="180"}
   sed -i -e "s/<UPLOAD_MAX_SIZE>/$UPLOAD_MAX_SIZE/g" /etc/nginx/nginx.conf
-  sed -i -e "s/<PHP_TIMEOUT>/$PHP_TIMEOUT/g" /etc/nginx/conf.d/default.conf
+  sed -i -e "s/<PHP_TIMEOUT>/$PHP_TIMEOUT/g" /etc/nginx/conf.d/02-default.conf
 
 
